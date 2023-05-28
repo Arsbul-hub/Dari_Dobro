455014c614be5d9f3338dc5f6f3adb42f4e825ef
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2023-05-28 14:21:02 +0300

    Добавлены анимации

---
+++ app/static/js/base.js
@@ -0,0 +1,25 @@
+function open_top_menu() {
+    document.getElementById("navigation").style.display = "block";
+    document.getElementById("navigation").style.animation = "base_animation 1s forwards";
+    document.getElementById("open-navigation").style.display = "none";
+    document.getElementById("close-navigation").style.display = "block";
+
+
+    document.getElementById("before").style.display = "none";
+    document.getElementById("after").style.display = "none";
+
+
+}
+
+function close_top_menu() {
+    document.getElementById("navigation").style.display = "none";
+    document.getElementById("navigation").style.animation = "base_animation 1s forwards";
+    document.getElementById("open-navigation").style.display = "block";
+    document.getElementById("close-navigation").style.display = "none";
+    document.getElementById("before").style.display = "block";
+    document.getElementById("after").style.display = "block";
+
+    // document.getElementsByClassName("navigation")[0].style.visibility = "hidden";
+    // document.body.style.overflowY = "auto";
+
+}
