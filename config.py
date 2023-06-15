5186e807deb6288ecb70e3304d6783c6f76e2d67
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2023-06-15 17:25:18 +0300

    Фиксы багов со стилями в новостях

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'n7546gd4792s94j842c8'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(
         os.path.abspath(os.path.dirname(__file__)), 'app.db')
 
